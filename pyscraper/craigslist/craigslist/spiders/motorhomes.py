6e280a6d2e45d5fced3fc8e23d8aedc8646fd40e
Miles <cluelesscoderny@gmail.com>
2019-06-28 16:09:19 -0400

    first scraper built

--- pyscraper/craigslist/craigslist/spiders/motorhomes.py
+++ pyscraper/craigslist/craigslist/spiders/motorhomes.py
@@ -8,4 +8,17 @@ class MotorhomesSpider(scrapy.Spider):
     start_urls = ['https://newyork.craigslist.org/search/rva?postedToday=1']
 
     def parse(self, response):
-        pass
+        listings = response.xpath('//li[@class="result-row"]')
+        for listing in listings:
+            date = listing.xpath(
+                './/*[@class="result-date"]/@datetime').extract_first()
+            link = listing.xpath(
+                './/a[@class="result-title hdrlnk"]/@href').extract_first()
+            text = listing.xpath(
+                './/a[@class="result-title hdrlnk"]/text()').extract_first()
+
+            yield {
+                'date': date,
+                'link': link,
+                'text': text
+            }
