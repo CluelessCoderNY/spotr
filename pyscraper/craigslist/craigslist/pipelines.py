a7992610512b4b2d516675970814bcc39840d1a8
Miles <cluelesscoderny@gmail.com>
2019-07-05 20:41:00 -0400

    added functionality to detect duplicate posts

--- pyscraper/craigslist/craigslist/pipelines.py
+++ pyscraper/craigslist/craigslist/pipelines.py
@@ -13,12 +13,31 @@ from scrapy.conf import settings
 class MongoDBPipeline(object):
 
     def __init__(self):
-        connection = MongoClient(
-            settings['MONGODB_SERVER'],
-            settings['MONGODB_PORT'])
-        db = connection[settings['MONGODB_DB']]
-        self.collection = db[settings['MONGODB_COLLECTION']]
+        try:
+            connection = MongoClient(
+                settings['MONGODB_SERVER'],
+                settings['MONGODB_PORT'])
+            db = connection[settings['MONGODB_DB']]
+            self.collection = db[settings['MONGODBdb_COLLECTION']]
+        except:
+            connection = MongoClient(
+                "localhost",
+                27017)
+            db = connection["spotr"]
+            self.collection = db["posts"]
 
     def process_item(self, item, spider):
-        self.collection.insert(dict(item))
+        found_item = self.collection.find_one({"link": item['link']})
+
+        if found_item == None:
+            self.collection.insert_one(item)
+
         return item
+
+
+if __name__ == "__main__":
+    testitem = {}
+    testitem['link'] = 'somelink.com'
+
+    pipeline = MongoDBPipeline()
+    processed_item = pipeline.process_item(testitem, None)
